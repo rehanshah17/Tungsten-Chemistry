469312c7dbc6fb5cb5f4bbad40ff915f6a92d8fe
Rehan Shah <rehans0906@gmail.com>
2024-05-06 19:13:56 -0400

    installed firebase

--- tungsten-chemistry/src/App.js
+++ tungsten-chemistry/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import React from 'react';
+
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import NavBar from './components/NavBar';
 import './App.css'
 import Home from './components/pages/Home'
 import Puzzle from './components/pages/Puzzle'
+import Battle from './components/pages/Battle'
 
 function App() {
   return (
@@ -14,6 +16,7 @@ function App() {
       <Routes>
         <Route path='/' exact Component={Home}/>
         <Route path='/puzzles' exact Component={Puzzle}/>
+        <Route path='/battle' exact Component={Battle}/>
       </Routes>
     </Router>  
     </>
---
+++ tungsten-chemistry/src/components/pages/Battle.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../../App.css';
+import Footer from '../FootNote.js';
+
+function Battle() {
+  return (
+    <>
+    <p>
+        These are some battles
+    </p>
+      <Footer />
+    </>
+  );
+}
+
+export default Battle;
