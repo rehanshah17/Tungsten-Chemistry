94bf926d7c34a7db0edf013598c49b2eccbcfb68
leon-chien <chienleonllc@gmail.com>
2024-05-28 20:39:22 -0400

    Populated grade page

--- tungsten-chemistry/src/components/pages/Grades.js
+++ tungsten-chemistry/src/components/pages/Grades.js
@@ -1,10 +1,46 @@
-import React from 'react';
+// components/pages/Grades.js
+import React, { useEffect, useState } from 'react';
+import { collection, getDocs, query, orderBy } from 'firebase/firestore';
+import { db } from '../../firebase';
+import '../Grades.css';
 
 function Grades() {
+  const [responses, setResponses] = useState([]);
+
+  useEffect(() => {
+    const fetchResponses = async () => {
+      const q = query(collection(db, 'responses'), orderBy('question'));
+      const querySnapshot = await getDocs(q);
+      const responsesData = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+      setResponses(responsesData);
+    };
+
+    fetchResponses();
+  }, []);
+
   return (
     <div className="gradesPage">
-      <h1>Grades Page</h1>
-      {/* Add your content here */}
+      <h1>Grades</h1>
+      <table className="gradesTable">
+        <thead>
+          <tr>
+            <th>Question</th>
+            <th>Response</th>
+            <th>Difficulty</th>
+            <th>User</th>
+          </tr>
+        </thead>
+        <tbody>
+          {responses.map((response) => (
+            <tr key={response.id}>
+              <td>{response.question}</td>
+              <td>{response.response}</td>
+              <td>{response.difficulty}</td>
+              <td>{response.user.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
